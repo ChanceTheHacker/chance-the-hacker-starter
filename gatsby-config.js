fc2952eda2a493f62e9da313594da9c48e9619dc
William <ertyjunior@gmail.com>
2019-10-29 16:50:02 -0500

    loaded plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,11 @@
+const config = require("./config/config")
+const plugins = require("./config/plugins")
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Chance The Hacker's Starter`,
+    description: `A starter featuring Typescript, Sass, React Spring, React Waypoint and Google Analytics built in. Brought to you by the fine folks working hard at The Hacking Corp, working hard, so you don't have to!`,
     author: `@gatsbyjs`,
   },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+  plugins,
 }
